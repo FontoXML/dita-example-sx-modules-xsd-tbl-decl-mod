09b0b5948f9c2f4249c34c940aa891f7f8b14f44
eva <eva.de.haas@liones.nl>
2019-06-28 11:06:36 +0200

    Upgrade to SDK 7.7

--- src/configureSxModule.js
+++ src/configureSxModule.js
@@ -1,95 +1,85 @@
-define([
-	'fontoxml-families/configureProperties',
-	'fontoxml-families/createElementMenuButtonWidget',
-	'fontoxml-families/createMarkupLabelWidget',
-	'fontoxml-localization/t',
-	'fontoxml-table-flow-cals/configureAsCalsTableElements'
-], function (
-	configureProperties,
-	createElementMenuButtonWidget,
-	createMarkupLabelWidget,
-	t,
-	configureAsCalsTableElements
-) {
-	'use strict';
+import configureProperties from 'fontoxml-families/src/configureProperties.js';
+import createElementMenuButtonWidget from 'fontoxml-families/src/createElementMenuButtonWidget.js';
+import createMarkupLabelWidget from 'fontoxml-families/src/createMarkupLabelWidget.js';
+import t from 'fontoxml-localization/src/t.js';
+import configureAsCalsTableElements from 'fontoxml-table-flow-cals/src/configureAsCalsTableElements.js';
 
-	return function configureSxModule (sxModule) {
-		// CALS table
-		configureAsCalsTableElements(sxModule, {
-			table: {
-				localName: 'table'
-			},
-			entry: {
-				defaultTextContainer: 'p'
-			}
-		});
+export default function configureSxModule(sxModule) {
+	// CALS table
+	configureAsCalsTableElements(sxModule, {
+		table: {
+			localName: 'table'
+		},
+		entry: {
+			defaultTextContainer: 'p'
+		}
+	});
 
-		// colspec
-		//     The <colspec> element contains a column specification for a table, including assigning a column name
-		//     and number, cell content alignment, and column width. Category: Table elements
-		configureProperties(sxModule, 'self::colspec', {
-			markupLabel: t('columns')
-		});
+	// colspec
+	//     The <colspec> element contains a column specification for a table, including assigning a column name
+	//     and number, cell content alignment, and column width. Category: Table elements
+	configureProperties(sxModule, 'self::colspec', {
+		markupLabel: t('columns')
+	});
 
-		// entry
-		//     The <entry> element defines a single cell in a table. Category: Table elements
-		configureProperties(sxModule, 'self::entry', {
-			contextualOperations: [
-				{ name: 'contextual-column-insert' },
-				{ name: 'contextual-column-after-insert' },
-				{ name: 'contextual-column-delete' },
-				{ name: 'contextual-row-insert' },
-				{ name: 'contextual-row-after-insert' },
-				{ name: 'contextual-row-delete' }
-			],
-			markupLabel: t('cell')
-		});
-		configureProperties(sxModule, 'self::entry[fonto:get-column-index(.) = 0 and ancestor::table[1][@rowheader="firstcol"]]', {
+	// entry
+	//     The <entry> element defines a single cell in a table. Category: Table elements
+	configureProperties(sxModule, 'self::entry', {
+		contextualOperations: [
+			{ name: 'contextual-column-insert' },
+			{ name: 'contextual-column-after-insert' },
+			{ name: 'contextual-column-delete' },
+			{ name: 'contextual-row-insert' },
+			{ name: 'contextual-row-after-insert' },
+			{ name: 'contextual-row-delete' }
+		],
+		markupLabel: t('cell')
+	});
+	configureProperties(
+		sxModule,
+		'self::entry[fonto:get-column-index(.) = 0 and ancestor::table[1][@rowheader="firstcol"]]',
+		{
 			backgroundColor: 'black'
-		});
+		}
+	);
 
-		// row
-		//     The <row> element contains a single row in a table <tgroup>. Category: Table elements
-		configureProperties(sxModule, 'self::row', {
-			markupLabel: t('row')
-		});
+	// row
+	//     The <row> element contains a single row in a table <tgroup>. Category: Table elements
+	configureProperties(sxModule, 'self::row', {
+		markupLabel: t('row')
+	});
 
-		// table
-		configureProperties(sxModule, 'self::table', {
-			contextualOperations: [
-				{ name: ':cals-table-insert-title' },
-				{ name: ':cals-table-insert-desc' },
-				{ name: 'cals-table-delete' }
-			],
-			markupLabel: t('table figure'),
-			titleQuery: './title',
-			blockHeaderLeft: [
-				createMarkupLabelWidget()
-			],
-			blockOutsideAfter: [
-				createElementMenuButtonWidget()
-			]
-		});
+	// table
+	configureProperties(sxModule, 'self::table', {
+		contextualOperations: [
+			{ name: ':cals-table-insert-title' },
+			{ name: ':cals-table-insert-desc' },
+			{ name: 'cals-table-delete' }
+		],
+		markupLabel: t('table figure'),
+		titleQuery: './title',
+		blockHeaderLeft: [createMarkupLabelWidget()],
+		blockOutsideAfter: [createElementMenuButtonWidget()]
+	});
 
-		// tbody
-		//     The <tbody> element contains the rows in a table. Category: Table elements
-		configureProperties(sxModule, 'self::tbody', {
-			markupLabel: t('body')
-		});
+	// tbody
+	//     The <tbody> element contains the rows in a table. Category: Table elements
+	configureProperties(sxModule, 'self::tbody', {
+		markupLabel: t('body')
+	});
 
-		// tgroup
-		//     The <tgroup> element in a table contains column, row, spanning, header and footer specifications,
-		//     and the body (<tbody>) of the table. Category: Table elements
-		configureProperties(sxModule, 'self::tgroup', {
-			markupLabel: t('table'),
-			tabNavigationItemSelector: 'self::entry'
-		});
+	// tgroup
+	//     The <tgroup> element in a table contains column, row, spanning, header and footer specifications,
+	//     and the body (<tbody>) of the table. Category: Table elements
+	configureProperties(sxModule, 'self::tgroup', {
+		markupLabel: t('table'),
+		tabNavigationItemSelector: 'self::entry'
+	});
 
-		// thead
-		//     The table header (<thead>) element precedes the table body (<tbody>) element in a complex table.
-		//     Category: Table elements
-		configureProperties(sxModule, 'self::thead', {
-			markupLabel: t('header')
-		});
-	};
-});
+	// thead
+	//     The table header (<thead>) element precedes the table body (<tbody>) element in a complex table.
+	//     Category: Table elements
+	configureProperties(sxModule, 'self::thead', {
+		markupLabel: t('header')
+	});
+}
